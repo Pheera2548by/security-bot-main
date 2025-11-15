2456ae7916b42d2eed09f437b7d942ed4b365bb1
Pheera2548by <za0870799560@gmail.com>
2025-11-16 02:37:51 +0700

    Fix port configuration

--- server.js
+++ server.js
@@ -244,7 +244,7 @@ app.get('/', (req, res) => {
 });
 
 // Start Server
-const PORT = process.env.PORT || 8080;
+const PORT = "8888"
 app.listen(PORT, () => {
     console.log('🚀 Server started on port', PORT);
     console.log('🗃️ Database: Supabase PostgreSQL');
